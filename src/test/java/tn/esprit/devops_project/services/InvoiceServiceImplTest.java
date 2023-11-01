a04e378271324a6d5f40d1c7c043361cd4cf0957
samar bouzazi <69418214+samarbouzazi@users.noreply.github.com>
2023-11-01 22:34:40 +0100

    Update InvoiceServiceImplTest.java

--- src/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
@@ -51,12 +51,12 @@ class InvoiceServiceImplTest {
     @Test
     @DatabaseSetup("/data-set/invoice-data.xml")
     void cancelInvoice() {
-        Long invoiceId = 1L; // Adjust with the invoice ID from your test data
+        Long invoiceId = 1L; 
 
-        // Act - Call the cancelInvoice method
+
         invoiceService.cancelInvoice(invoiceId);
 
-        // Assert - Verify that the invoice is archived
+      
         final Invoice invoice = this.invoiceService.retrieveInvoice(invoiceId);
         assertNotNull(invoice);
         assertTrue(invoice.getArchived());
@@ -69,47 +69,14 @@ class InvoiceServiceImplTest {
         final Invoice inv = this.invoiceService.retrieveInvoice(1L);
         assertNotNull(inv);
         assertEquals(1L, inv.getIdInvoice());
-    //    assertEquals("label 1", inv.getLabel()); // Adjust the expected size based on your test data
+    
     }
 
-    @Test
-    @DatabaseSetup("/data-set/invoice-data.xml")
-    @DatabaseSetup("/data-set/supplier-data.xml")
-    void getInvoicesBySupplier() {
-       /* // To Re-Check
-        // Retrieve the supplier with ID 1
-        Supplier supplier = supplierService.retrieveSupplier(1L);
-
-        assertNotNull(supplier, "Supplier should not be null");
-
-        // Retrieve all invoices
-        List<Invoice> allInvoices = invoiceService.retrieveAllInvoices();
-
-        // Check that the invoices belong to the correct supplier
-        for (Invoice invoice : allInvoices) {
-            assertEquals(supplier, invoice.getSupplier(),
-                    "Invoice should belong to the correct supplier");
-        }*/
-    }
+   
 
 
-    @Test
-    @DatabaseSetup("/data-set/invoice-data.xml")
-    @DatabaseSetup("/data-set/operator-data.xml")
-    void assignOperatorToInvoice() {
-        /*final Operator operator = this.operatorService.retrieveOperator(1L);
-        final Invoice invoice = this.invoiceService.retrieveInvoice(1L);
-        this.invoiceService.assignOperatorToInvoice(1L, 1L);
-        assertEquals(1, operator.getInvoices().size());*/
-    }
 
-    @Test
-    @DatabaseSetup("/data-set/invoice-data.xml")
-    void getTotalAmountInvoiceBetweenDates() {
-
-    }
-
-    // Exception 1
+   
     @Test
     @DatabaseSetup("/data-set/invoice-data.xml")
     void retrieveInvoice_nullId() {
