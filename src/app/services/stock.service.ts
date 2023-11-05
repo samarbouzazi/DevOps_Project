c3e989edfbd32d257b41107dafc73f986a83e1a9
samar bouzazi <69418214+samarbouzazi@users.noreply.github.com>
2023-11-05 15:37:11 +0100

    Update stock.service.ts

--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -6,7 +6,7 @@ import {Observable} from 'rxjs';
   providedIn: 'root'
 })
 export class StockService {
-  private apiUrl = 'http://192.168.153.128:8082/stock';
+  private apiUrl = 'http://192.168.153.128:8088/stock';
 
   constructor(private http: HttpClient) {
   }
