2dec7645dba8355bc292afa788b1f7d2ef04ce96
samarbouzezi <samar.bouzezi@esprit.tn>
2023-11-06 15:47:18 +0100

    correct tests

--- src/main/java/tn/esprit/devops_project/services/InvoiceDetailService.java
+++ src/main/java/tn/esprit/devops_project/services/InvoiceDetailService.java
@@ -1,8 +1,28 @@
+package tn.esprit.devops_project.services;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import tn.esprit.devops_project.entities.Invoice;
+import tn.esprit.devops_project.entities.Operator;
+import tn.esprit.devops_project.entities.Supplier;
+import tn.esprit.devops_project.repositories.InvoiceDetailRepository;
+import tn.esprit.devops_project.repositories.InvoiceRepository;
+import tn.esprit.devops_project.repositories.OperatorRepository;
+import tn.esprit.devops_project.repositories.SupplierRepository;
+import tn.esprit.devops_project.services.Iservices.IInvoiceService;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Slf4j
+@AllArgsConstructor
 public class InvoiceDetailService {
 
     private InvoiceDetailRepository invoiceDetailRepository;
 
-    public InvoiceDetailService(InvoiceDetailRepository invoiceDetailRepository) {
+    public InvoiceDetailService(invoiceDetailRepository invoiceDetailRepository) {
         this.invoiceDetailRepository = invoiceDetailRepository;
     }
 
