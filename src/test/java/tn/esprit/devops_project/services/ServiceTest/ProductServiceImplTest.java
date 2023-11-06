39c26771885a3a81f0bd2e831fd91d96b611487f
samar bouzazi <69418214+samarbouzazi@users.noreply.github.com>
2023-11-06 14:39:39 +0100

    Update ProductServiceImplTest.java

--- src/test/java/tn/esprit/devops_project/services/ServiceTest/ProductServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/ServiceTest/ProductServiceImplTest.java
@@ -1,34 +1,19 @@
 package tn.esprit.devops_project.services.ServiceTest;
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
-import tn.esprit.devops_project.controllers.OperatorController;
-import tn.esprit.devops_project.entities.Operator;
 import tn.esprit.devops_project.entities.Product;
 import tn.esprit.devops_project.entities.ProductCategory;
 import tn.esprit.devops_project.entities.Stock;
-import tn.esprit.devops_project.repositories.StockRepository;
+import tn.esprit.devops_project.repositories.ProductRepository;
 import tn.esprit.devops_project.services.ProductServiceImpl;
 import tn.esprit.devops_project.services.StockServiceImpl;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,43 +21,20 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 @Transactional
-@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
-        TransactionalTestExecutionListener.class,
-        DbUnitTestExecutionListener.class})
-@ActiveProfiles("test")
-
 public class ProductServiceImplTest {
     @Autowired
     private ProductServiceImpl productService;
     @Autowired
     private StockServiceImpl stockService;
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private StockRepository stockRepository;
 
-
-
-
-
-    // @DatabaseSetup("/data-set/product-data.xml")
-    // @DatabaseSetup("/data-set/stock-data.xml")
-    // @Test
-    // void addProduct() {
-    //     final Product product = new Product();
-    //     Stock stock = this.stockService.retrieveStock(1L);
-    //     product.setStock(stock);
-    //     // product.setIdProduct(2L);
-    //     product.setTitle("t2");
-    //     product.setPrice(2);
-    //     product.setQuantity(2);
-    //     product.setCategory(ProductCategory.ELECTRONICS);
-    //     this.productService.addProduct(product, stock.getIdStock());
-    //     assertEquals(2, this.productService.retreiveAllProduct().size());
-    // }
-
-         @Test
+    @Test
     public void testAddProduct_WithValidProductAndIdStock_ShouldReturnSavedProduct() {
         Product product = new Product("t1", "Product Description", 1.0, 1);
         Long idStock = 1L;
@@ -101,48 +63,41 @@ public class ProductServiceImplTest {
     }
 
     @Test
-    @DatabaseSetup("/data-set/product-data.xml")
-    void retrieveProduct() {
-        final Product product = this.productService.retrieveProduct(1L);
-        assertEquals("t1", product.getTitle());
+    public void retrieveProduct() {
+        Long id = 1L;
+        Product product = new Product("t1", "Product Description", 1.0, 1);
+
+        Mockito.when(productRepository.findById(id)).thenReturn(Optional.of(product));
+
+        Product retrievedProduct = productServiceImpl.retrieveProduct(id);
+
+        assertEquals(id, retrievedProduct.getId());
+        assertEquals("t1", retrievedProduct.getTitle());
+        assertEquals("Product Description", retrievedProduct.getDescription());
+        assertEquals(1.0, retrievedProduct.getPrice(), 0.01);
+        assertEquals(1, retrievedProduct.getQuantity());
+        assertEquals("ELECTRONICS", retrievedProduct.getCategory());
+
+        Mockito.verify(productRepository).findById(id);
     }
-    @Test
-    @DatabaseSetup("/data-set/product-data.xml")
-    void retrieveProduct_NullId() {
+
+    @Test(expected = NullPointerException.class)
+    public void retrieveProduct_NullId() {
         Exception exception = assertThrows(NullPointerException.class, () -> {
             final Product product = this.productService.retrieveProduct(100L);
         });
     }
 
     @Test
-    @DatabaseSetup("/data-set/product-data.xml")
-    void retreiveAllProduct() {
-        final List<Product> allProducts = this.productService.retreiveAllProduct();
-        assertEquals(1, allProducts.size());
-    }
+    public void retreiveAllProduct() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Product Name 1", "Product Description 1", 100.0, 1));
+        products.add(new Product("Product Name 2", "Product Description 2", 150.0, 2));
 
-    @Test
-    @DatabaseSetup("/data-set/product-data.xml")
-    void retrieveProductByCategory() {
-        final List<Product> electronicsProducts = this.productService.retrieveProductByCategory(ProductCategory.ELECTRONICS);
-        assertEquals(1, electronicsProducts.size());
-    }
+        Mockito.when(productRepository.findAll()).thenReturn(products);
 
-    @DatabaseSetup("/data-set/product-data.xml")
-    @DatabaseSetup("/data-set/stock-data.xml")
-    @Test
-    void retrieveProductStock() {
-        Stock stock = this.stockService.retrieveStock(1L);
-        final List<Product> allProducts = this.productService.retreiveProductStock(2L);
-        for (Product product : allProducts){
-            assertEquals(product.getStock(),stock);
-        }
-    }
-    @DatabaseSetup("/data-set/product-data.xml")
-    @Test
-    void deleteOperator() {
-        Product product = this.productService.retrieveProduct(1L);
-        this.productService.deleteProduct(product.getIdProduct());
-    }
+        List<Product> retrievedProducts = productServiceImpl.retreiveAllProduct();
 
-}
+        assertEquals(products.size(), retrievedProducts.size());
+        for (int i = 0; i < retrievedProducts.size(); i++) {
+            assertEquals(products.get(i).getId(), retrieved
