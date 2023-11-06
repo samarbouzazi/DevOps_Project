e6293a91cec1bf0e9088b23b2c53204314e6254f
samar bouzazi <69418214+samarbouzazi@users.noreply.github.com>
2023-11-06 13:44:03 +0100

    Update StockServiceImplTest.java

--- src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
@@ -1,22 +1,3 @@
-package tn.esprit.devops_project.services;
-
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-import org.springframework.transaction.annotation.Transactional;
-import tn.esprit.devops_project.entities.Stock;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
@@ -48,6 +29,7 @@ class StockServiceImplTest {
     @DatabaseSetup("/data-set/stock-data.xml")
     void retrieveStock() {
         final Stock stock = this.stockService.retrieveStock(1L);
+        assertNotNull(stock);
         assertEquals("stock 1", stock.getTitle());
     }
 
@@ -55,15 +37,33 @@ class StockServiceImplTest {
     @DatabaseSetup("/data-set/stock-data.xml")
     void retrieveAllStock() {
         final List<Stock> allStocks = this.stockService.retrieveAllStock();
-        assertEquals(1 ,allStocks.size() );
+        assertEquals(1, allStocks.size());
+    }
 
+    @Test
+    @DatabaseSetup("/data-set/stock-data.xml")
+    void updateStock() {
+        Stock stockToUpdate = this.stockService.retrieveStock(1L);
+        stockToUpdate.setTitle("UpdatedTitle");
+        stockService.updateStock(stockToUpdate);
+
+        final Stock updatedStock = this.stockService.retrieveStock(1L);
+        assertEquals("UpdatedTitle", updatedStock.getTitle());
+    }
+
+    @Test
+    @DatabaseSetup("/data-set/stock-data.xml")
+    void deleteStock() {
+        final Stock stock = this.stockService.retrieveStock(1L);
+        stockService.deleteStock(stock.getIdStock());
+        assertNull(this.stockService.retrieveStock(1L));
     }
 
     @Test
     @DatabaseSetup("/data-set/stock-data.xml")
     void retrieveStock_nullId() {
-        Exception exception = assertThrows(NullPointerException.class, () -> {
-            final Stock stock = this.stockService.retrieveStock(100L);
+        assertThrows(NullPointerException.class, () -> {
+            final Stock stock = this.stockService.retrieveStock(null);
         });
     }
 }
