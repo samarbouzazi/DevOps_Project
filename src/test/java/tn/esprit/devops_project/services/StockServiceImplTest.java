084a7266e0c122165ae7c277b9eaa2cc6477fdac
samar bouzazi <69418214+samarbouzazi@users.noreply.github.com>
2023-11-06 13:47:47 +0100

    Update StockServiceImplTest.java

--- src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
+++ src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
@@ -1,3 +1,22 @@
+package tn.esprit.devops_project.services;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import tn.esprit.devops_project.entities.Stock;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
